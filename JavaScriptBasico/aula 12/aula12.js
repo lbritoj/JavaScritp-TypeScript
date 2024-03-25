6839f1d63a174a239e65e3b8489bc8a5ed1a4bd9
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-03-25 00:14:13 -0400

    class 13

--- JavaScriptBasico/aula 12/aula12.js
+++ JavaScriptBasico/aula 12/aula12.js
@@ -1,5 +1,7 @@
 console.log('Oi')
 console.log('Este trecho será exibido no console do navegador');
+
+alert("Está acordado?")
 //é possível executar o node no terminal do windows.
 
 //Boas praticas => separar o html do JavaScript
@@ -7,3 +9,6 @@ console.log('Este trecho será exibido no console do navegador');
 //Não é boa prática colocar a tag scrip dentro do head do HTML, pois irá retardar o carregamento da página.
 //A melh9or pratica é colocar o script no final do body
 
+//MEsmo os navegadores que possuem a mesma engine de interpreatação de JS, eles nãop agem igual
+
+//Não é possível usar o alert no nodeJS ... somente nos navegadores;
