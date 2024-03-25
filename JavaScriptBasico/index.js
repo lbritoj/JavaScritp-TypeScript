ea6472e0ae1f1d18900f342003d6209505ed60e0
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-03-24 23:59:48 -0400

    first exercise

---
+++ JavaScriptBasico/index.js
@@ -0,0 +1,16 @@
+//Exibir uma mensagem no console
+// O ; não é obrigatório
+console.log('Leonardo Brito');
+console.log("Leonardo Brito");
+console.log(`Leonardo Brito`);
+console.log('Ele disse que "é possível usar aspas"');
+
+//Pode-se usar aspas simples ou duplas ou com o uso das crases.
+// deve-se fechar com o mesmo tipo da abertura.
+// para separar, basta usar a vírgula ... as casas decimais são separadas com o ponto (.)
+
+console.log(35, 15.85, 'Leonardo Brito de novo')
+
+//Em JS tudo é number .. não existe separação entre inteiro e ponto flutuante;
+//JS é uma linguagem de tipagem dinâmica;
+
