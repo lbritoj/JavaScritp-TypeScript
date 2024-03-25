574403ed343775e23dc464ca4803b798f21bdaa7
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-03-25 00:20:43 -0400

    folder aula12 renamed

---
+++ JavaScriptBasico/aula 13/index.js
@@ -0,0 +1,12 @@
+let nome; //Declarou a variável
+nome = 'João'; //Inicializando a variável
+
+//Var caiu em desuso, atualmente as variáveis devem ser criadas usando o LET em JS
+// Não é possível declarar duas vezes a mesma variável
+//Regras: não pode criar variável com palavras reservadas;
+//        não pode iniciar com número;
+//        precisam ter nomes significativos;
+//        
+console.log(nome)
+console.log('Em 2000', nome, 'conheceu Maria.')
+console.log(nome, 'casou-se com Maria em 2012.')
