32dcf31a5f75307c7e9da58ff8cd27887a3f1d4a
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-03-25 00:09:26 -0400

    link script

---
+++ JavaScriptBasico/aula 12/aula12.js
@@ -0,0 +1,9 @@
+console.log('Oi')
+console.log('Este trecho será exibido no console do navegador');
+//é possível executar o node no terminal do windows.
+
+//Boas praticas => separar o html do JavaScript
+
+//Não é boa prática colocar a tag scrip dentro do head do HTML, pois irá retardar o carregamento da página.
+//A melh9or pratica é colocar o script no final do body
+
