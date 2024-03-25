ac790cad1c1566e4af49a84637d22aa7a7fb4f75
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-03-25 00:33:26 -0400

    class 14 finished

---
+++ JavaScriptBasico/aula 14/index.js
@@ -0,0 +1,21 @@
+const nome = 'João';
+
+//Ao declarar uma constante, ela deve ser inicializada imediatamente!!!
+
+// É possível atribuir o valor de uma constante a uma variável
+
+const primeiroNumero = 5;
+const segundoNumero = '10';
+const conta = primeiroNumero + segundoNumero;
+
+console.log(conta)
+
+//Para saber o tipo de um dado em JS usar o typeof
+
+console.log(typeof primeiroNumero);
+console.log(typeof segundoNumero);
+
+//quando existe uma string e usamos o sinal de +, ocorrerá uma concatenação;
+//a soma só ocorrerá quando houver apenas números.
+
+//o resultado da concatenação de um número com uma string, será uma string.
