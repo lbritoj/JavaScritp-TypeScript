b1d91cf0699a8b2e3c7fc51c973849ff55f465d9
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-03-25 00:25:27 -0400

    class 14

--- JavaScriptBasico/aula 13/index.js
+++ JavaScriptBasico/aula 13/index.js
@@ -6,7 +6,14 @@ nome = 'João'; //Inicializando a variável
 //Regras: não pode criar variável com palavras reservadas;
 //        não pode iniciar com número;
 //        precisam ter nomes significativos;
-//        
+//        não podem conter espaços ou traços;
+//        JS é Case Sensitive; nomeCliete <> nomecliente
+//  NÃO UTILIZAR VAR, SEEEMPRE UTILIZAR LET;
 console.log(nome)
 console.log('Em 2000', nome, 'conheceu Maria.')
 console.log(nome, 'casou-se com Maria em 2012.')
+
+let nomeCliente = 'Leonardo';
+let nomecliente = 'Brito'
+
+console.log(nomeCliente, nomecliente)
